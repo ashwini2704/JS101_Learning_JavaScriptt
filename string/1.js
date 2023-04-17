10b97a79bd457b028fe8337dbc14c532ce59d95e
ashwini2704 <ashwini.titarmare96@gmail.com>
2023-04-18 00:16:31 +0530

    8th

---
+++ string/1.js
@@ -0,0 +1,10 @@
+let a= "prerna is a good girl";
+
+let space=0;
+
+for(let i=0; i<a.length; i++) {
+  if(a[i]==" ") {
+    space++;
+  }
+}
+console.log("word : ",space+1);
---
+++ string/2.js
@@ -0,0 +1,8 @@
+let a=["masai","school","teacher","lecture","video"];
+
+let count_character=0;
+
+for(let i=0; i<a.length; i++) {
+  count_character=count_character + a[i].length
+}
+console.log(count_character);
