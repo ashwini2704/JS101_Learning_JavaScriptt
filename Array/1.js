0ed613f20972ec7b4bbdf0beeec80b465065c16b
ashwini2704 <ashwini.titarmare96@gmail.com>
2023-04-18 00:14:16 +0530

    7th

---
+++ Array/1.js
@@ -0,0 +1,5 @@
+let cousin = ["prerna", "sonu","aman", "monti", "mahi", "meet","shrushti", "laxmi"]
+
+for(let i=0; i<cousin.length; i++) {
+  console.log(i+1,cousin[i]);
+}
---
+++ Array/3.js
@@ -0,0 +1,11 @@
+let number= [1,2,3,4,5,6,7,8,9];
+let sum=0;
+let count=0;
+
+for(let i=0; i<=number.length; i++) {
+  if(number[i] %2==0) {
+    sum=sum+number[i];
+    count++;
+  }
+}
+console.log(sum/count);
